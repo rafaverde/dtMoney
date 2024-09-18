4de8b6afe66cb58edb9e5f9da365d45e354b0051
rafaverde <rafaverde@msn.com>
2024-09-18 13:17:25 -0300

    Transactions context created.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react"
+import { useContext } from "react"
+import { TransactionsContext } from "../../contexts/TransactionsContext"
+
 import { Header } from "../../components/Header"
 import { SearchForm } from "../../components/SearchForm"
 import { Summary } from "../../components/Summary"
@@ -8,28 +10,8 @@ import {
   TransactionsTable,
 } from "./styles"
 
-interface Transaction {
-  id: number
-  description: string
-  type: "income" | "outcome"
-  category: string
-  price: number
-  createdAt: string
-}
-
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions")
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
