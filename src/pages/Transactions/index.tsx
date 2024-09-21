00ed550eecaed777b7970dcd3a1f4e49753bd155
rafaverde <rafaverde@msn.com>
2024-09-21 07:13:47 -0300

    Prices and dates formatted using Intl.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -9,6 +9,7 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from "./styles"
+import { dateFormatter, priceFormatter } from "../../utils/formatter"
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext)
@@ -27,11 +28,12 @@ export function Transactions() {
                 <td width="50%">{transaction.description}</td>
                 <td width="25%">
                   <PriceHighlight $variant={transaction.type}>
-                    {`R$ ${transaction.price.toFixed(2).replace(".", ",")}`}
+                    {transaction.type === "outcome" && "- "}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>
